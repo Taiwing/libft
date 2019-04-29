667b25173b61b95f1e52d51c9a5c67cfff43dc4b
Yoann FOREAU <yforeau@e3r11p8.42.fr>
2019-04-29 18:00:18 +0200

    Normify ft_collector and correct gnl errors

--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,20 +6,20 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 17:48:27 by yforeau           #+#    #+#             */
-/*   Updated: 2019/04/29 17:50:12 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/04/29 17:59:35 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "get_next_line.h"
 
-static int		ft_isfd(void *fd, void *content)
+int		ft_isfd(void *fd, void *content)
 {
 	return (*(int *)fd != ((t_gnl *)content)->fd);
 }
 
 #ifdef NO_COLLEC
 
-void			rm_cur(t_list **lst, t_gnl *cur)
+void	rm_cur(t_list **lst, t_gnl *cur)
 {
 	ft_lst_remove_if(lst, (void *)&(cur->fd), ft_isfd);
 	free(cur);
@@ -27,7 +27,7 @@ void			rm_cur(t_list **lst, t_gnl *cur)
 
 #else
 
-void			rm_cur(t_list **lst, t_gnl *cur)
+void	rm_cur(t_list **lst, t_gnl *cur)
 {
 	ft_lst_remove_if(lst, (void *)&(cur->fd), ft_isfd);
 	free(ft_heap_collector(cur, FT_COLLEC_GET));
