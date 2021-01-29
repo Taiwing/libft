d4bcf09daf8e2c9edb184b503453fd22252fc414
yoann foreau <yoann.foreau@ito.am>
2021-01-29 14:53:17 +0100

    Fix ft_strncmp possible buffer overflow
    This has been here for a long time... I was not expecting this.
    Anyway, it's always good to fix the lib.

--- src/str/ft_strncmp.c
+++ src/str/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 18:26:23 by yforeau           #+#    #+#             */
-/*   Updated: 2018/11/08 11:06:04 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/01/29 14:53:03 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	t_uchar	c1;
 	t_uchar	c2;
 
-	c1 = (t_uchar)*s1;
-	c2 = (t_uchar)*s2;
 	if (!n)
 		return (0);
-	else if (c1 != c2 || c1 == 0 || c2 == 0)
+	c1 = (t_uchar)*s1;
+	c2 = (t_uchar)*s2;
+	if (c1 != c2 || c1 == 0 || c2 == 0)
 		return (c1 - c2);
 	else
 		return (ft_strncmp(s1 + 1, s2 + 1, n - 1));
