a963c3d060b84badbf954916a0521ed533d8635e
yoann foreau <yoann.foreau@ito.am>
2021-04-15 20:58:33 +0200

    Update bint header comment documentation
    This is clearer and more exhaustive. Also sets up the future
    sign handling for bints.

--- includes/bint.h
+++ includes/bint.h
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 12:01:14 by yforeau           #+#    #+#             */
-/*   Updated: 2021/04/15 20:32:44 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/04/15 20:57:35 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,31 @@
 # include <stdint.h>
 
 /*
-** size 36 makes it possible to represent up to 1120 bits unsigned integers
-** size 520 makes it possible to represent up to 16608 bits unsigned integers
+** Bint numbers are signed integers of arbitrary length
 **
-** BINT_LEN is the number of blocks occupied by the number (0 means value 0)
+** They are represented by an array of uint32_t numbers which are its digits
+** stored in a 'small-endian' fashion. This means that the first number block
+** (after the info block at index 0) is the smallest 32bit digit.
+**
+** The info block contains the number 'meta data'. It has its size encoded
+** which is the maximum number of number blocks it can contain plus one for
+** the info block (so it's simply its underlying array size).
+**
+** BINT_LEN - the number of blocks occupied by the number (0 means value 0)
 ** max: 7FFF = (2^15)-1 = 32767
 ** This means a bint number can be at most 1048544 bits long, and have an
 ** absolute value up to (2^1048544)-1. It can represent (2^1048545)-1 values
 ** (two times more than its length).
 **
-** BINT_SIZE total size of the array (so total number of blocks + 1)
+** BINT_SIGN - the sign (1 for negative, 0 for positive) TODO
+**
+** BINT_SIZE - total size of the array (so total number of blocks + 1)
 ** max: FFFF = (2^16)-1 = 65535
 ** Of course the maximum useful value is BINT_LEN max + 1 (2^15 = 32768)
 ** because it is the maximum theoretical space needed to represent a bint.
 **
-** BINT_SIGN will be the sign (1 for negative, 0 for positive) TODO
+** BINT_SIZE of 36 makes it possible to represent 1120 bits integers
+** BINT_SIZE of 520 makes it possible to represent 16608 bits integers
 **
 ** Every bint function will return BINT_FAILURE on failure and BINT_SUCCESS
 ** otherwise. The two exceptions are bintcmp which has a strcmp type return
@@ -38,7 +48,6 @@
 */
 
 # define BINT_SIZE_DEF			520
-# define SIZE_UINTMAX			2
 # define BINT_LEN_MASK			0x00007FFF
 # define BINT_SIGN_MASK			0x00008000
 # define BINT_SIZE_MASK			0xFFFF0000
