3d7da2b04adab487839966bd83c3da418e515591
yoann foreau <yoann.foreau@ito.am>
2021-11-18 10:10:23 +0100

    Fix retarded strlcat
    It's still a little bit retarded, but it's clearer now.

--- src/str/ft_strlcat.c
+++ src/str/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 18:25:58 by yforeau           #+#    #+#             */
-/*   Updated: 2018/11/08 09:41:53 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/11/18 10:08:26 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,19 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	const char	*s;
-	char		*d;
 	size_t		dlen;
+	size_t		slen;
 
-	s = src;
-	d = dest;
-	while (size && *d)
+	if ((dlen = (size_t)ft_strlen(dest)) >= size)
+		return (size + ft_strlen(src));
+	size -= dlen;
+	if ((slen = (size_t)ft_strlen(src) + 1) > size)
+		slen = size;
+	while (*src && size > 1)
 	{
-		d++;
-		size--;
+		*dest++ = *src++;
+		--size;
 	}
-	dlen = d - dest;
-	if (size == 0)
-		return (dlen + ft_strlen(src));
-	while (*s)
-	{
-		if (size != 1)
-		{
-			*d++ = *s;
-			size--;
-		}
-		s++;
-	}
-	*d = 0;
-	return (dlen + (s - src));
+	*dest = 0;
+	return (dlen + slen);
 }
