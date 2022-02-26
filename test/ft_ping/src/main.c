a3a9197ba7a262d5281d526e231ce1d39c2e8753
yoann foreau <yoann.foreau@ito.am>
2022-02-26 07:24:38 +0100

    test: multiple hosts on ping scan

--- test/ft_ping/src/main.c
+++ test/ft_ping/src/main.c
@@ -6,23 +6,27 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 05:43:30 by yforeau           #+#    #+#             */
-/*   Updated: 2022/02/26 06:34:07 by yforeau          ###   ########.fr       */
+/*   Updated: 2022/02/26 07:16:29 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	print_result(t_scanres *result)
+static void	print_result(t_scanres *result, t_scan scan)
 {
-	ft_printf("\nResult:\n");
+	ft_printf("\nResult (%d):\n", scan);
 	ft_printf("rtt: tv_sec = %lld, tv_usec = %lld\n",
 		result->rtt.tv_sec, result->rtt.tv_usec);
 	ft_printf("ttl: %hhu\n", result->ttl);
 	ft_printf("open: %s\n", result->open ? "true" : "false");
 	ft_printf("reason: %d\n", result->reason);
 	ft_printf("sequence: %hu\n", result->sequence);
-	ft_printf("sequence from scan_control: %hu\n",
-		g_scan_list[E_FTSCAN_ECHO_PING][0]->sequence);
+
+	// DEBUG
+	t_scan_control	*scan_ctrl;
+	if (!(scan_ctrl = ft_get_scan(scan)))
+		ft_exit(EXIT_FAILURE, "ft_get_scan: %s", ft_strerror(ft_errno));
+	ft_printf("sequence from scan_control: %hu\n", scan_ctrl->sequence);
 }
 
 static void	print_events(uint16_t events)
@@ -55,27 +59,59 @@ static void	print_events(uint16_t events)
 	ft_printf("\n");
 }
 
+static void	send_probes(t_pollsc *scans, int host_count)
+{
+	for (int i = 0; i < host_count; ++i)
+		if (ft_scan_send(scans[i].scan) < 0)
+			ft_exit(EXIT_FAILURE, "ft_scan_send: %s", ft_strerror(ft_errno));
+}
+
+static int	get_results(int *done, t_pollsc *scans, int host_count)
+{
+	int			count = 0;
+	t_scanres	result = { 0 };
+
+	for (int i = 0; i < host_count; ++i)
+	{
+		if (scans[i].events || done[i])
+			++count;
+		if (!scans[i].events || done[i])
+			continue;
+		ft_printf("\n");
+		print_events(scans[i].events);
+		if (ft_scan_result(&result, scans[i].scan) < 0)
+			ft_exit(EXIT_FAILURE, "ft_scan_result: %s", ft_strerror(ft_errno));
+		print_result(&result, scans[i].scan);
+		done[i] = 1;
+	}
+	return (count == host_count);
+}
+
+#define	MAX_ARGS	256
+
 int	main(int argc, char **argv)
 {
-	t_ip			ip;
 	int				ret;
-	t_scan			scan;
-	t_pollsc		scans = { 0 };
-	t_scanres		result = { 0 };
-	char			*host = argv[1];
+	int				done[MAX_ARGS] = { 0 };
+	t_ip			ip[MAX_ARGS] = { 0 };
+	int				host_count = argc - 1;
+	t_pollsc		scans[MAX_ARGS] = { 0 };
 	struct timeval	timeout = { 5, 0 };
 	char			buf[INET6_ADDRSTRLEN];
 
-	(void)argc;
-	if (!host)
+	if (!host_count)
 		ft_exit(EXIT_FAILURE, "Usage: %s host...", argv[0]);
-	if ((ret = ft_get_ip(&ip, host, AF_UNSPEC)) < 0)
-		ft_exit(EXIT_FAILURE, "ft_get_ip: %s", gai_strerror(ret));
-	ft_printf("IP: %s\n", inet_ntop(ip.family, ft_ip_addr(&ip),
-		buf, INET6_ADDRSTRLEN));
 	ft_atexit(ft_scan_close_all);
-	if ((scan = ft_echo_ping_open(&ip, &timeout)) < 0)
-		ft_exit(EXIT_FAILURE, "ft_echo_ping_open: %s", ft_strerror(ft_errno));
+	for (int i = 0; i < host_count; ++i)
+	{
+		if ((ret = ft_get_ip(ip + i, argv[i + 1], AF_UNSPEC)) < 0)
+			ft_exit(EXIT_FAILURE, "ft_get_ip: %s", gai_strerror(ret));
+		ft_printf("IP: %s\n", inet_ntop(ip[i].family, ft_ip_addr(ip + i),
+			buf, INET6_ADDRSTRLEN));
+		if ((scans[i].scan = ft_echo_ping_open(ip + i, &timeout)) < 0)
+			ft_exit(EXIT_FAILURE, "ft_echo_ping_open: %s",
+				ft_strerror(ft_errno));
+	}
 	/*
 	while (!(ret = ft_echo_ping(&result, scan)))
 	{
@@ -86,20 +122,16 @@ int	main(int argc, char **argv)
 		ft_exit(EXIT_FAILURE, "ft_echo_ping: %s", ft_strerror(ft_errno));
 	ft_scan_close(scan);
 	*/
-	if (ft_scan_send(scan) < 0)
-		ft_exit(EXIT_FAILURE, "ft_scan_send: %s", ft_strerror(ft_errno));
-	scans.scan = scan;
-	while ((ret = ft_scan_poll(&scans, 1, NULL)) >= 0)
+	send_probes(scans, host_count);
+	while ((ret = ft_scan_poll(scans, host_count, NULL)) >= 0)
 	{
 		if (!ret) continue;
-		ft_printf("\n");
-		print_events(scans.events);
-		if (ft_scan_result(&result, scan) < 0)
-			ft_exit(EXIT_FAILURE, "ft_scan_result: %s", ft_strerror(ft_errno));
-		print_result(&result);
-		sleep(1);
-		if (ft_scan_send(scan) < 0)
-			ft_exit(EXIT_FAILURE, "ft_scan_send: %s", ft_strerror(ft_errno));
+		if (get_results(done, scans, host_count))
+		{
+			ft_bzero(done, sizeof(done));
+			sleep(1);
+			send_probes(scans, host_count);
+		}
 	}
 	ft_exit(EXIT_FAILURE, "ft_scan_poll: %s", ft_strerror(ft_errno));
 	return (EXIT_SUCCESS);
